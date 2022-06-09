887345ed78ad6a1fa3d9f4eb0cb3513d3b93ec1b
thiagolages <thiagolagesrocha@gmail.com>
2022-06-09 17:01:44 -0300

    Add loading animation and finalize screenshot button.

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,20 +1,46 @@
 import html2canvas from "html2canvas";
-import { Camera } from "phosphor-react";
+import { Camera, Trash } from "phosphor-react";
 import { useState } from "react";
+import { Loading } from "../Loading";
 
-export function ScreenshotButton(){
+interface ScreenShotButtonProps{
+    screenshot: string | null;
+    onScreenShotTake: (screenshot: string | null) => void;
+}
+                   // instead of props: ScreenShotButtonProps
+export function ScreenshotButton({ screenshot, onScreenShotTake }: ScreenShotButtonProps){
 
     const [isTakingScreenshot, setIsTakingScreenshot] = useState(false); // to show loading image while the screenshot is being taken
 
     async function handleTakeScreenshot(){
-
         setIsTakingScreenshot(true);
-
+        
         // ! at the end forces it to 'accept' that it will always find a 'html' tag. The function by default considers the case where we don't find any html tag
         const canvas = await html2canvas(document.querySelector('html')!); 
-        const base64iamge = canvas.toDataURL('image/png'); // convert the image to a base64 textual representation of it
+        const base64image = canvas.toDataURL('image/png'); // convert the image to a base64 textual representation of it
+
+        onScreenShotTake(base64image); // instead of props.onScreenShotTake(base64image);
 
         setIsTakingScreenshot(false);
+    }
+
+    if (screenshot){
+        return (
+            <button
+                type="button"
+                className="p-1 h-10 w-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-colors"
+                style={{
+                    backgroundImage: `url(${screenshot})`,
+                    // next 2 lines just to show something on the preview, since our website is black.
+                    // if using on real applications, remove them !
+                    backgroundPosition: 'right bottom',
+                    backgroundSize: 180,
+                }}
+                onClick={() => onScreenShotTake(null)} // whenever we click  the trash button, set screenshot to null (erase it)
+                >
+                <Trash weight="fill"/>
+            </button>
+        );
     }
 
     return (
@@ -23,7 +49,7 @@ export function ScreenshotButton(){
         onClick={handleTakeScreenshot}
         className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 disabled:opacity-50 disabled:hover:bg-brand-500"
     >
-        <Camera className="w-6 h-6"/>
+        { isTakingScreenshot ? <Loading/> : <Camera className="w-6 h-6"/> }
     </button>
 );
 }
--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,5 @@
 import { ArrowLeft, Camera } from "phosphor-react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -12,7 +13,20 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
     // in order to access each element in the 'FeedbackType' object, we do:
     const  feedbackTypeInfo = feedbackTypes[props.feedbackType];
     
-    
+    const [screenshot, setScreenshot]   = useState<string | null>(null);
+    const [comment, setComment]         = useState<string | null>('');
+
+
+    function handleSubmitFeedback(event:FormEvent){
+
+        event.preventDefault(); // prevent the default behaviour of refreshing the page when submitting the form
+
+        console.log({
+            screenshot,
+            comment
+        });
+    }
+
     return (
         <>
             <header>
@@ -36,18 +50,25 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
             </header>
 
             {/* margin top and bottom */}
-            <form className="my-4 w-full">
+            <form 
+                onSubmit={handleSubmitFeedback}
+                className="my-4 w-full">
                 <textarea
                     className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Conte com detalhes o que está acontecendo..."
+                    onChange={ event => setComment(event.target.value)} // update comment with anything the user types
                 />
 
             <footer className="flex gap-2 mt-2">
 
-               <ScreenshotButton/>
+               <ScreenshotButton
+                screenshot={screenshot}
+                onScreenShotTake={setScreenshot}
+               />
 
                 <button
                     type="submit"
+                    disabled={comment?.length === 0}
                     className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                 >
                     Send feedback
